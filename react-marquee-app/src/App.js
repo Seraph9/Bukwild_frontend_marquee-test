995d994c8d25d365179606eff97d869d7c9cc904
Abdullah Wafy <red.sulphur786@gmail.com>
2021-03-16 18:07:01 -0700

    created layout-ui component as header rendering the logo

--- react-marquee-app/src/App.js
+++ react-marquee-app/src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
+// local imports
+import Header from './components/Header';
+
 function App() {
   return (
-    <h1>Hello world!</h1>
+    <div>
+      <Header />
+    </div>
   );
 }
 
---
+++ react-marquee-app/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div class='layout-ui'>
+            <div class='logo'>
+                <img src='images/abc.svg' class='abc' />
+            </div>
+        </div>
+    );
+};
+
+export default Header;
